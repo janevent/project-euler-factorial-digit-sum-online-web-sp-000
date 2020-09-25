46e3a7a17886cf000d01f28f88acf62667ec4746
Jane Venter <janeanneventer@gmail.com>
2020-09-25 22:14:49 +0000

    Automatically backed up by Learn

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,6 +1,6 @@
 # code your solution here
 def factorial(n) 
-  answer = (n..1).to_a.reverse.reduce(n){|sum, num| sum * num }
+  answer = (1..n).to_a.reverse().reduce(n){|sum, num| sum * num }
   puts n
   puts answer
   answer
